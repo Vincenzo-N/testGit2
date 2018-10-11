74b12b2a871eb3441181e158dda2ea187036e2a1
Tore95 <43842155+Tore95@users.noreply.github.com>
2018-10-11 20:39:09 +0200

    ciao

--- TEST2/src/test2.java
+++ TEST2/src/test2.java
@@ -2,5 +2,6 @@ public class test2 {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Hello World_2");
+        System.out.println("hello world 3");
     }
 }
